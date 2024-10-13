573005e5b9ee5b5a4e62533af63e2ef800a982ec
bejeju <bejejupark@gmail.com>
2024-10-13 20:29:58 +0900

    delete recorddashboard

--- lib/fixedexpense.dart
+++ lib/fixedexpense.dart
@@ -40,6 +40,8 @@ class _FixedExpenseState extends State<FixedExpense> {
   String recordsTypeTitle = "";
   double? heightFor;
   Map<String, double>? dataMap;
+  final List<Map<String, IconData>> _addCategory = AddCategory().addIcons;
+  final List<Map<String, IconData>> _minusCategory = MinusCategory().MinusIcons;
 
   @override
   void initState() {
@@ -90,13 +92,13 @@ class _FixedExpenseState extends State<FixedExpense> {
     // fetch totalFixedAmount
     await supabase
         .from('daily_record')
-        .select('amount, category')
+        .select('id, date, amount, category, info')
         .eq('user_uuid', storedId.toString())
         .gte('date', '$selectedYear-$selectedMonth-01') // 시작일 (1일)
         .lt('date', nextMonthStartDate.toIso8601String()) // 다음 월의 첫날 이전
         .or(categoryQuery)
         .then((recordAmounts) {
-      print("recordAmounts test : $recordAmounts");
+      print("recordAmounts : $recordAmounts");
       int totalAmountResult = 0;
       if (recordAmounts.isNotEmpty) {
         for (var recordAmount in recordAmounts) {
@@ -123,15 +125,44 @@ class _FixedExpenseState extends State<FixedExpense> {
     });
   }
 
-  IconData whatIconIs(String recordCategory) {
+  IconData whatIconIs(String recordCategory, String sign) {
     IconData result = const IconData(0xee80, fontFamily: 'MaterialIcons');
-    for (int i = 0; i < fixedIcons.length; i++) {
-      if (recordCategory == fixedIcons[i].keys.first) {
-        result = fixedIcons[i].values.first;
-        break;
+    if (sign == '+') {
+      for (int i = 0; i < _addCategory.length; i++) {
+        if (recordCategory == _addCategory[i].keys.first) {
+          result = _addCategory[i].values.first;
+          break;
+        }
       }
+    } else if (sign == '-') {
+      for (int i = 0; i < _minusCategory.length; i++) {
+        if (recordCategory == _minusCategory[i].keys.first) {
+          result = _minusCategory[i].values.first;
+          break;
+        }
+      }
+    } else if (sign == '') {
+      return result;
     }
 
+    return result;
+  }
+
+  Future<String> deleteRecord(int recordId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? storedId = prefs.getString('uuid');
+    String result = 'nothing';
+    await supabase
+        .from('daily_record')
+        .delete()
+        .eq('id', recordId)
+        .eq('user_uuid', storedId.toString())
+        .then((response) {
+      result = 'success';
+    }).catchError((error) {
+      print("deleted error : $error");
+      result = 'error';
+    });
     return result;
   }
 
@@ -150,30 +181,229 @@ class _FixedExpenseState extends State<FixedExpense> {
           (Match match) => '${match[1]},',
         );
 
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.09,
-      margin: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.02),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(5)),
-        border: Border.all(
-          color: Colors.black,
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Icon(
-                whatIconIs(dailyRecord['category'].toString()),
-                size: 23,
+    String sign = (dailyRecord['amount'] != null && dailyRecord['amount'] > 0)
+        ? '+'
+        : (dailyRecord['amount'] != null && dailyRecord['amount'] < 0)
+            ? '-'
+            : '';
+
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
               ),
-              Text(dailyRecord['category'].toString()),
-            ],
+              backgroundColor: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(
+                          whatIconIs(dailyRecord['category'].toString(), sign),
+                          size: MediaQuery.of(context).size.width * 0.09,
+                          // color: Colors.blue,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.03,
+                        ),
+                        Text(
+                          dailyRecord['category'],
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.08,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black, // 텍스트 색상
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8), // 요소 간 간격
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          dailyRecord['date'].toString(),
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.05,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey[600], // 회색 텍스트
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "$sign $formattedNumber 원",
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.07,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.blue, // 금액 색상
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          dailyRecord['info'].toString(),
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.05,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black87, // 다크 그레이 텍스트
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16), // 버튼과 텍스트 간격
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () async {
+                            String result =
+                                await deleteRecord(dailyRecord['id']);
+
+                            if (result == 'success') {
+                              return showDialog(
+                                context: context,
+                                barrierDismissible: true, // 모달 바깥을 클릭하면 닫히도록 설정
+                                builder: (context) {
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    backgroundColor: Colors.white,
+                                    child: Container(
+                                      padding: EdgeInsets.all(
+                                          MediaQuery.of(context).size.width *
+                                              0.03),
+                                      width: MediaQuery.of(context).size.width *
+                                          0.06,
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.1,
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          const Text("삭제 되었습니다."),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      FixedExpense(
+                                                    whatRecordsIs:
+                                                        widget.whatRecordsIs,
+                                                  ),
+                                                ),
+                                              );
+                                            },
+                                            style: TextButton.styleFrom(
+                                              textStyle: const TextStyle(
+                                                color: Colors.blue,
+                                              ),
+                                            ),
+                                            child: const Text('확인'),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ).then((value) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => FixedExpense(
+                                      whatRecordsIs: widget.whatRecordsIs,
+                                    ),
+                                  ),
+                                );
+                              });
+                            } else if (result == 'error') {
+                              return showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    backgroundColor: Colors.white,
+                                    child: Container(
+                                      padding: EdgeInsets.all(
+                                          MediaQuery.of(context).size.width *
+                                              0.03),
+                                      width: MediaQuery.of(context).size.width *
+                                          0.06,
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.06,
+                                      child: const Text('오류가 발생하였습니다.'),
+                                    ),
+                                  );
+                                },
+                              );
+                            }
+                          },
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          child: const Text('삭제'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.09,
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.02),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          border: Border.all(
+            color: Colors.black,
           ),
-          Text('- $formattedNumber 원'),
-        ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  whatIconIs(dailyRecord['category'].toString(), sign),
+                  size: 23,
+                ),
+                Text(dailyRecord['category'].toString()),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.02,
+                ),
+                Text(dailyRecord['date'].toString()),
+              ],
+            ),
+            Text('- $formattedNumber 원'),
+          ],
+        ),
       ),
     );
   }
